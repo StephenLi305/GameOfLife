451e5950456afbdbf7f609bda90ff24ea5b3f2fe
Stephen Li <stephen.li305@gmail.com>
2019-06-25 11:19:46 -0700

    created Main Compoenet

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Main extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            generation: 0,
+        };
+
+    }    
+    render(){
+        return(
+            <div>
+                <h1>The Game of Life</h1>
+                <Grid
+                />
+                <h2>Generation: {this.state.generation}</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
