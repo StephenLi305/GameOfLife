445b72cdedc5b0c06cd7faaff67a3c1b5723fe65
Stephen Li <stephen.li305@gmail.com>
2019-08-12 16:37:02 -0700

    Updates

---
+++ precache-manifest.0f4b1d81ed4dd0d31a9329d0e966a877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62e8ee1ecf2e05989a6ba6d2c310265",
+    "url": "/GameOfLife/index.html"
+  },
+  {
+    "revision": "20fabf33d9343d675cc6",
+    "url": "/GameOfLife/static/css/main.2429d857.chunk.css"
+  },
+  {
+    "revision": "6c7b2f6eeaee7cd597b2",
+    "url": "/GameOfLife/static/js/2.e815c7fe.chunk.js"
+  },
+  {
+    "revision": "20fabf33d9343d675cc6",
+    "url": "/GameOfLife/static/js/main.e770224d.chunk.js"
+  },
+  {
+    "revision": "5ac9ced736084ac7fdd8",
+    "url": "/GameOfLife/static/js/runtime~main.4a239a3b.js"
+  }
+]);
